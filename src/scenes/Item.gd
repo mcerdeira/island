a99babbcd00e86a07332b00d274a3ed4d1bc7fed
martin_cerdeira <Martin.Cerdeira@comafi.com.ar>
2024-04-17 15:08:41 -0300

    C

--- src/scenes/Item.gd
+++ src/scenes/Item.gd
@@ -3,22 +3,29 @@ var this_item = null
 
 func _ready():
 	$MenuSel.visible = false
+	$MenuItem.visible = false
+	$MenuItem2.visible = false
 	this_item = Global.pick_random(Global.ITEMS)
 	boot_start()
 	
 func boot_start():
 	$sprite.animation = this_item.name
-	$MenuSel.add_item(this_item.action)
-	$MenuSel.add_item("To Inventory")
-	$MenuSel.add_item("Leave")
+	$MenuItem.text = "== " + this_item.description + " =="
+	$MenuSel.add_item(this_item.action, null, false)
+	$MenuSel.add_item("To Inventory", null, false)
+	$MenuSel.add_item("Leave", null, false)
 
 func _on_area_entered(area):
 	if area.is_in_group("players_area"):
 		$MenuSel.visible = true
+		$MenuItem.visible = true
+		$MenuItem2.visible = true
 		get_tree().paused = true
 
 func _on_menu_sel_item_clicked(index, at_position, mouse_button_index):
 	$MenuSel.visible = false
+	$MenuItem.visible = false
+	$MenuItem2.visible = false
 	await get_tree().create_timer(0.1).timeout
 	get_tree().paused = false
 	do_action(index)
@@ -34,3 +41,5 @@ func do_action(index):
 func _on_area_exited(area):
 	if area.is_in_group("players_area"):
 		$MenuSel.visible = false
+		$MenuItem.visible = false
+		$MenuItem2.visible = false
